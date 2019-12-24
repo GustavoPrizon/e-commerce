3b6340ebcacf7016464b6fc7c8c65079eb471481
Gustavo Prizon <gustavoprizon@iMac-de-Gustavo.local>
2019-12-24 12:51:46 -0300

    removendo o produto do carrinho

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,9 +1,9 @@
-import producer from 'immer';
+import produce from 'immer';
 
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return producer(state, draft => {
+      return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.product.id);
 
         if (productIndex >= 0) {
@@ -13,6 +13,14 @@ export default function cart(state = [], action) {
             ...action.product,
             amount: 1,
           });
+        }
+      });
+    case 'REMOVE_FROM_CART':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
         }
       });
     default:
